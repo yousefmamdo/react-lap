3d42ae8c1a4fb8481b40e536009f45aa560d21d5
yousefmamdo <yousefmando1998@gmail.com>
2021-08-29 14:27:54 +0300

    finsh lap1

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class Footer extends React.Component{
+
+    render(){
+        return(
+            <>
+
+<span>&copy;yousefmando</span>
+            </>
+        )
+    }
+
+}
+export default Footer;
