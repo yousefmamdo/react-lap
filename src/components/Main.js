0bb4d8aa1e4a7711eca5f9c64efd6c04999ed326
yousefmamdo <yousefmando1998@gmail.com>
2021-09-06 01:07:12 +0300

    fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,14 +9,14 @@ class Main extends React.Component{
 
 <HornedBeasts 
 title='UniWhal'
-imageUrl= 'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg'
-description='A unicorn and a narwhal nuzzling their horns'
+imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
+description = "A unicorn and a narwhal nuzzling their horns"
 
 />
 <HornedBeasts
 title='Unicorn Head'
-imageUrl=  'https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg'
-description='Someone wearing a creepy unicorn head mask'
+imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
+description = "Mother (or father) rhino with two babies"
 
 />
             </>
